5d10f19dc482caefb9f282903fe416e1e002ba69
Linus Unnebäck <linus@folkdatorn.se>
2024-10-16 12:29:52 +0200

    wip

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
 function App(): React.JSX.Element {
+  const [result, setResult] = useState<string>('');
+
+  useEffect(async () => {
+    try {
+      const response = await fetch('https://httpbin.org/cookies/set?x=x&y=y');
+      const cookies = response.headers.getSetCookie();
+      setResult(JSON.stringify(cookies, undefined, 2));
+    } catch (error) {
+      setResult(error.stack);
+    }
+  }, [])
+
   return (
     <SafeAreaView>
-      <Text style={styles.result}>Hello World</Text>
+      <Text style={styles.result}>{result}</Text>
     </SafeAreaView>
   );
 }
